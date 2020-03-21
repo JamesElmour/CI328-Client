b9c67b6c386543177a00f80b64b444f7bd928988
James Elmour <j.elmour1@uni.brighton.ac.uk>
2020-03-21 11:54:11 +0000

    Finalizing rudementary networked player movement.

--- game/network/NetworkReciever.js
+++ game/network/NetworkReciever.js
@@ -8,8 +8,17 @@ class NetworkReciever extends NetworkTransceiver
     playerMove(data)
     {
         let position = data[0];
+        
+        if(data.length === 2)
+        {
+            let number = new Uint16Array(new Uint8Array([data[0], data[1]]).buffer.slice(0))
+            position = number[0];
+        }
+
+        console.log("Position: " + position);
 
         if(window.pigm.scene !== undefined)
-            window.pigm.scene.systems[0].components[0].parent.position.x = position;
+            if(window.pigm.scene.systems[0].components.length !== 0)
+                window.pigm.scene.systems[0].components[0].parent.position.x = position;
     }
 }
--- game/scenes/TestScene.js
+++ game/scenes/TestScene.js
@@ -9,22 +9,32 @@ class TestScene extends Scene
     {
         super.createSystems();
 
-        this.PlayerSystem = new NetworkedPlayerSystem({keyboard: this.keyboard, mouse: this.mouse});
+        this.PlayerSystem   = new NetworkedPlayerSystem({keyboard: this.keyboard, mouse: this.mouse});
+        this.SpriteRenderer = new SpriteRenderer({canvas: this.canvas, camera: this.camera});
         this.systems.push(this.PlayerSystem);
-
-        this.createPlayer();
+        this.systems.push(this.SpriteRenderer);
     }
 
     loadImages()
     {
         this.il.loadImage("entities/player/Debug.png");
+
+        let imagesLoaded = () => window.setTimeout(() => (this.il.loadingIndex == 0) ? this.loaded() : imagesLoaded(), 32);
+        imagesLoaded();
+    }
+
+    loaded()
+    {
+        this.createPlayer();
     }
 
     createPlayer()
     {
         let entity = new Entity({name: "Player", position: new Vector2(300, 300)});
         let player = entity.createComponent(NetworkedPlayer, {});
+        let sprite = entity.createComponent(Sprite, {image: this.il.getImage("entities/player/Debug.png")});
 
         this.PlayerSystem.addComponent(player);
+        this.SpriteRenderer.addComponent(sprite);
     }
 }
