9773efad983c2930d1a22c425c2d35ce5d6f9912
James Elmour <j.elmour1@uni.brighton.ac.uk>
2020-03-23 14:51:16 +0000

    Adding player and brick bounce with combo system.

--- game/systems/NetworkedBallSystem.js
+++ game/systems/NetworkedBallSystem.js
@@ -3,12 +3,16 @@ class NetworkedBallSystem extends System
     constructor(opts)
     {
         super(opts);
+
+        this.ComboCount = 0;
     }
 
     process(ball)
     {
-        this.move(ball);
+        this.playerBounce(ball);
         this.wallBounce(ball);
+        this.brickBounce(ball);
+        this.move(ball);
     }
 
     move(ball)
@@ -20,6 +24,48 @@ class NetworkedBallSystem extends System
 
         position.x += velocity.x;
         position.y += velocity.y;
+    }
+
+    brickBounce(ball)
+    {
+        let collider = ball.parent.getComponent(Collider);
+
+        if(this.hasCollidedWith(collider, "Brick"))
+        {
+            let brickIndex = collider.collidedTags.indexOf("Brick");
+            let brick = collider.collidedWith[brickIndex];
+            let brickPosition = brick.parent.position;
+            let collisionVector = new Vector2(ball.parent.position.x - (brickPosition.x + 32), ball.parent.position.y - brickPosition.y).normalize();
+            
+            if(Math.abs(collisionVector.x) > 0.70)
+            {
+                ball.direction.x = -ball.direction.x;
+
+                if(collisionVector.x < 0)
+                {
+                    ball.parent.position.x = brickPosition.x;
+                }
+                else
+                {
+                    ball.parent.position.x = brickPosition.x + 64;
+                }
+            }
+            else
+            {
+                ball.direction.y = -ball.direction.y;
+
+                if(collisionVector.y < 0)
+                {
+                    ball.parent.position.y = brickPosition.y;
+                }
+                else
+                {
+                    ball.parent.position.y = brickPosition.y + 16;
+                }
+            }
+            
+            this.ComboCount++;
+        }
     }
 
     wallBounce(ball)
@@ -35,5 +81,29 @@ class NetworkedBallSystem extends System
             ball.direction.y = -ball.direction.y;
             ball.parent.position.y = Math.max(Math.min(ball.parent.position.y, 720), 360);
         }
+    }
+
+    playerBounce(ball)
+    {
+        let collider = ball.parent.getComponent(Collider);
+
+        if(this.hasCollidedWith(collider, "Player"))
+        {
+            ball.parent.position.y = 656;
+            ball.direction.y = -ball.direction.y;
+
+            let player = collider.collidedWith[collider.collidedTags.indexOf("Player")].parent.getComponent(NetworkedPlayer);
+            let playerPos = player.parent.position.x;
+            let newDirection = (((ball.parent.position.x - playerPos) / 256) -0.5);
+            ball.direction.x = newDirection;
+
+            window["pigm"].scene.PowerUpSystem.GetPowerUp(this.ComboCount);
+            this.ComboCount = 0;
+        }
+    }
+
+    hasCollidedWith(collider, tag)
+    {
+        return collider.collidedTags.indexOf(tag) !== -1;
     }
 }
